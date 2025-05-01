e94189780a34a0c04118fbc3dccd8b49dcaf9b3b
Luis Monzon <60436830+Luis4609@users.noreply.github.com>
2025-05-01 12:14:03 +0200

    feat: add theme-provider and dark mode as default

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import { useAccounts } from "@/shared/hooks/useAccounts"; // Import the custom hook
 import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import AccountsPage from "./domains/accounts/AccountsPage";
@@ -6,51 +7,60 @@ import DcfCalculatorPage from "./domains/dcf-calculator/DcfCalculatorPage";
 
 function App() {
   // Use the custom hook to manage accounts state and logic
-  const { accounts, addAccount, updateAccountBalance, deleteAccount } =
-    useAccounts();
+  const { accounts, addAccount, updateAccount, deleteAccount } = useAccounts();
 
   return (
-    <Router>
-      <div className="App">
-        <nav className="bg-gray-800 p-4">
-          <ul className="flex space-x-4">
-            <li>
-              <Link to="/dashboard" className="text-white hover:text-gray-300">
-                Dashboard
-              </Link>
-            </li>
-            <li>
-              <Link to="/accounts" className="text-white hover:text-gray-300">
-                Accounts
-              </Link>
-            </li>
-            <li>
-              <Link to="/dcf-calculator" className="text-white hover:text-gray-300">DCF Calculator</Link> {/* Add link */}
-            </li>
-          </ul>
-        </nav>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Router>
+        <div className="App">
+          <nav className="bg-gray-800 p-4">
+            <ul className="flex space-x-4">
+              <li>
+                <Link
+                  to="/dashboard"
+                  className="text-white hover:text-gray-300"
+                >
+                  Dashboard
+                </Link>
+              </li>
+              <li>
+                <Link to="/accounts" className="text-white hover:text-gray-300">
+                  Accounts
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/dcf-calculator"
+                  className="text-white hover:text-gray-300"
+                >
+                  DCF Calculator
+                </Link>
+              </li>
+            </ul>
+          </nav>
 
-        <Routes>
-          <Route
-            path="/dashboard"
-            element={<DashboardPage accounts={accounts} />}
-          />
-          <Route
-            path="/accounts"
-            element={
-              <AccountsPage
-                accounts={accounts}
-                onAddAccount={addAccount}
-                onUpdateBalance={updateAccountBalance}
-                onDeleteAccount={deleteAccount}
-              />
-            }
-          />
-          <Route path="/dcf-calculator" element={<DcfCalculatorPage />} />
-          <Route path="/" element={<DashboardPage accounts={accounts} />} />
-        </Routes>
-      </div>
-    </Router>
+          <Routes>
+            <Route
+              path="/dashboard"
+              element={<DashboardPage accounts={accounts} />}
+            />
+            <Route
+              path="/accounts"
+              element={
+                <AccountsPage
+                  accounts={accounts}
+                  onAddAccount={addAccount}
+                  onUpdateBalance={updateAccount}
+                  onDeleteAccount={deleteAccount}
+                />
+              }
+            />
+            <Route path="/dcf-calculator" element={<DcfCalculatorPage />} />
+            <Route path="/" element={<DashboardPage accounts={accounts} />} />
+          </Routes>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
